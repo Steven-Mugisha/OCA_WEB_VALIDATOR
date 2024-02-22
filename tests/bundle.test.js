924475884961522598f6b964816366d4269a39d1
smizero <steven.mizero@contech.univeristy>
2024-02-22 16:41:15 -0500

    oca web validator lib.

---
+++ tests/bundle.test.js
@@ -0,0 +1,86 @@
+import e from "express";
+import OCADataSet from "../lib/utils/files.js";
+import { OCADataSetErr } from "../lib/validator.js";
+import { OCABundle } from "../lib/validator.js";
+
+// describe("OCADataSet", () => {
+//   it("should return dataset in json format for an excel spreadsheet", async () => {
+//     const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/data_entry.xlsx`);
+//     console.log(dataset);
+//     expect(dataset).toBeInstanceOf(Object);
+//   });
+// });
+
+describe("OCABundle", () => {
+  it("should return a json object for a bundle file.", async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    expect(bundle).toBeInstanceOf(OCABundle);
+  });
+});
+
+describe("OCABundle", () => {
+  it("should return a json object for the overlay specified.", async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const overlay = bundle.getOverlay("label");
+    expect(overlay).toBeInstanceOf(Object);
+  });
+});
+
+
+describe("OCABundle", () => {
+  it("should return a version number for the bundle.", async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const version = bundle.getOverlayVersion("label");
+    expect( typeof version).toBe("string");
+  });
+});
+
+describe("OCABundle", () => {
+  it("should return an object of attributes define in the schema bundle." , async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const attributes = bundle.getAttributes();
+    expect(attributes).toBeInstanceOf(Object);
+  });
+});
+
+describe("OCABundle", () => {
+  it("should return a type of an attribute as a string." , async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const attrType = bundle.getAttributeType("Age");
+    expect(typeof attrType).toBe("string");
+  });
+});
+
+describe("OCABundle", () => {
+  it("should return a type of an attribute as a string." , async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const attrFormat = bundle.getAttributeFormat("Age");
+    expect(typeof attrFormat).toBe("string");
+  });
+});
+
+describe("OCABundle", () => {
+  it("should return a M (Mandatory) or O (optional) if attribute is required." , async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const attrComformance = bundle.getAttributeConformance("Age");
+    expect(typeof attrComformance).toBe("string");
+    ;
+  });
+});
+
+describe("OCABundle", () => {
+  it("should return an object of entry codes." , async () => {
+    const bundle = new OCABundle();
+    await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+    const entryCodes = bundle.getAttributeEntryCodes("Breed");
+    expect(entryCodes).toBeInstanceOf(Object);
+
+  });
+});
