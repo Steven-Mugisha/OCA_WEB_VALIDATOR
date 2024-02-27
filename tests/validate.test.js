925c8fd9f182c5fc5723f1acc752113511e3e90b
smizero <steven.mizero@contech.univeristy>
2024-02-27 16:10:33 -0500

    validate function.

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -1,8 +1,9 @@
+import OCADataSetErr from "../lib/utils/Err.js";
 import OCADataSet from "../lib/utils/files.js";
 import OCABundle from "../lib/validator.js";
 
 describe("OCADataSet", () => {
-    it("should return dataset in json format from a csv file", async () => {
+    it("should return dataset in json format from a csv file.", async () => {
         const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/data_entry.xlsx`);
         expect(dataset).toBeInstanceOf(Object);
     });
@@ -18,22 +19,62 @@ describe("OCABundle", () => {
 
 // validating attributes
 describe("OCABundle", () => {
-    it("should return an array with unmatched attributes",  async () => {
+    it("should return an array with unmatched attributes.",  async () => {
         const bundle = new OCABundle();
         await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
-        const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/data_entry.xlsx`);
+        const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/err_data_entry.xlsx`);
         const unmatchedAttributes = bundle.validateAttribute(dataset);
+        // console.log(unmatchedAttributes);
         expect(unmatchedAttributes).toBeInstanceOf(Array);
     });
 });
 
 describe("OCABundle", () => {
-    it("should return an object with format errors",  async () => {
+    it("should return an object with format errors.",  async () => {
         const bundle = new OCABundle();
         await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
         const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/data_entry.xlsx`);
         const formatErrors = bundle.validateFormat(dataset);
-        // console.log(formatErrors);
         expect(formatErrors).toBeInstanceOf(Object);
     });
 });
+
+describe("OCABundle", () => {
+    it("should return an object with unmatched entry codes.",  async () => {
+        const bundle = new OCABundle();
+        await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+        const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/data_entry.xlsx`);
+        // console.log(dataset);
+        const entryCodeErrors = bundle.validateEntryCodes(dataset, "Breed");
+        expect(entryCodeErrors).toBeInstanceOf(Object);
+    });
+});
+
+describe("OCABundle", () => {
+    it("should return an array warning messages for flagged attributes.",  async () => {
+        const bundle = new OCABundle();
+        await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+        const flaggedAttributes = bundle.flaggedAlarm();
+        expect(flaggedAttributes).toBeInstanceOf(Object);
+    });
+});
+
+describe("OCABundle", () => {
+    it("should return an a string of a warning about mismatching overlay version and OCA bundle.",  async () => {
+        const bundle = new OCABundle();
+        await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+        const versionAlarm = bundle.versionAlarm();
+        expect(typeof versionAlarm).toBe("string");
+    });
+});
+
+describe("OCABundle", () => {
+    it("should return an object rslt with all issues to correct.",  async () => {
+        const bundle = new OCABundle();
+        await bundle.loadedBundle(`${__dirname}/datasets/oca_bundle.json`);
+        const dataset = await OCADataSet.readExcel(`${__dirname}/datasets/err_data_entry.xlsx`);
+        const validate = bundle.validate(dataset);
+        // console.log(validate);
+        expect(validate).toBeInstanceOf(Object);
+    });
+})
