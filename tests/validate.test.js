12c8f9fc21432ebd8d424cbed7ead46d04658d16
smizero <steven.mizero@contech.univeristy>
2024-03-14 13:21:03 -0400

    adding an error collection object.

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -7,46 +7,42 @@ describe('OCABundle', () => {
     it('should return OCADataSetErr object with all errors corrected.', async () => {
         const bundle = new OCABundle();
         await bundle.loadedBundle(path.join(__dirname, 'datasets', 'oca_bundle.json'));
-        const dataset = await OCADataSet.readExcel(path.join(__dirname, 'datasets', 'err_data_entry.xlsx'));
+        const dataset = await OCADataSet.readExcel(path.join(__dirname, 'datasets', 'data_entry.xlsx'));
         const validate = bundle.validate(dataset);
+        console.log(validate);
 
         /**
-         * The following logging statements can be used for debugging purposes.
-         *
-         * console.log(validate);
-         * console.log(validate.attErr);
-         * console.log(validate.formatErr);
-         * console.log(validate.entryCodeErr);
-         * console.log(validate.getErrCol("Age"));
-         * console.log(validate.getfirstErrCol());
-         * console.log(validate.getErrCol("Breed"));
-         * console.log(validate.missingAttrs);
-         * console.log(validate.unmachedAttrs);
-         *
-         *
-         * Example of the expect output object for the err_data_entry.xlsx.
-         *
-         * OCADataSetErr {
-         *    attErr: AttributeErr {
-         *        errs: [ [Array], [Array], [Array], [Array], [Array] ]
-         *    },
-         *    formatErr: FormatErr {
-         *        errs: {
-         *        Age: [Object],
-         *        BreastWt: {},
-         *        Breed: [Object],
-         *        Farm: [Object],
-         *        Glucose: [Object],
-         *        Lipase: {},
-         *        LiveWt: {}
-         *        }
-         *    },
-         *    entryCodeErr: EntryCodeErr { errs: { Breed: [Object] } },
-         *    missingAttrs: Set(2) { 'Lipase', 'LiveWt' },
-         *    unmachedAttrs: Set(3) { 'WtLive', 'coseGlu', 'Famr' },
-         *    errCols: Set(4) { 'Age', 'Breed', 'Farm', 'Glucose' },
-         *    errRows: Set(5) { '1', '2', '3', '4', '0' }
-         * }
+         *   OCADataSetErr {
+                attErr: AttributeErr {
+                    errs: [ [Array], [Array], [Array], [Array], [Array] ]
+                },
+                formatErr: FormatErr {
+                    errs: {
+                    Age: {},
+                    BreastWt: {},
+                    Breed: [Object],
+                    Farm: {},
+                    Glucose: [Object],
+                    Lipase: {},
+                    LiveWt: {}
+                    }
+                },
+                entryCodeErr: EntryCodeErr { errs: { Breed: [Object] } },
+                missingAttrs: Set(2) { 'Lipase', 'LiveWt' },
+                unmachedAttrs: Set(3) { 'WtLive', 'coseGlu', 'Famr' },
+                errCols: Set(2) { 'Breed', 'Glucose' },
+                errRows: Set(3) { '1', '6', '0' },
+                errCollection: {
+                    '0': { Glucose: 'Format mismatch. Supported format: [A-Z0-9]{9}.' },
+                    '1': {
+                    Breed: 'One of the entry codes required. Entry codes allowed: [B,S]'
+                    },
+                    '6': {
+                    Breed: 'One of the entry codes required. Entry codes allowed: [B,S]',
+                    Glucose: 'Format mismatch. Supported format: [A-Z0-9]{9}.'
+                    }
+                }
+            }
          */
 
         expect(validate).toBeInstanceOf(OCADataSetErr);
